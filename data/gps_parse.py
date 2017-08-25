71689c07d767609156e4287ad5e43c08c4ba5852
kiran4399 <kiran4399@gmail.com>
2017-08-24 22:09:41 -0700

    comments mod

--- data/gps_parse.py
+++ data/gps_parse.py
@@ -6,6 +6,8 @@ def parseGPS(str):
         msg = pynmea2.parse(str)
         print "Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)
 
+
+#replace the serial port and the baud rate with the desired values"
 serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
 
 
